538a297d586bf88eaf62879f576ff034ff6bad79
David Cumps <david@cumps.be>
2015-10-13 23:14:44 +0200

    Fix documentation

--- src/Exira.EventStore.Owin/AssemblyInfo.fs
+++ src/Exira.EventStore.Owin/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.EventStore.Owin")>]
 [<assembly: AssemblyProductAttribute("Exira.EventStore.Owin")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.EventStore.Owin is an OWIN middleware to keep an EventStore connection shared per application")>]
-[<assembly: AssemblyVersionAttribute("0.2.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.5")>]
-[<assembly: AssemblyMetadataAttribute("githash","078d973b9d18944ce2e8def3771f908517c54773")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyMetadataAttribute("githash","f0a161fc84ee2b67e9deb4a840be708177463c86")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.5"
+    let [<Literal>] Version = "0.2.0"
