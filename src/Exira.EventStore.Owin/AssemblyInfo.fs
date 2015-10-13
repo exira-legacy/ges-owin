2c48cdf780193177a479942f1baeac360d286729
Team City <info@exira.com>
2015-10-13 21:19:19 +0000

    Bump version to 0.2.6

--- src/Exira.EventStore.Owin/AssemblyInfo.fs
+++ src/Exira.EventStore.Owin/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.EventStore.Owin")>]
 [<assembly: AssemblyProductAttribute("Exira.EventStore.Owin")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.EventStore.Owin is an OWIN middleware to keep an EventStore connection shared per application")>]
-[<assembly: AssemblyVersionAttribute("0.2.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
-[<assembly: AssemblyMetadataAttribute("githash","f0a161fc84ee2b67e9deb4a840be708177463c86")>]
+[<assembly: AssemblyVersionAttribute("0.2.6")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.6")>]
+[<assembly: AssemblyMetadataAttribute("githash","538a297d586bf88eaf62879f576ff034ff6bad79")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.0"
+    let [<Literal>] Version = "0.2.6"
